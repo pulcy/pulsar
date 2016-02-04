6d93be2bb6a7c8bac2858ad33845a068ff5413f3
Ewout Prangsma <ewout@prangsma.net>
2016-02-04 10:28:41 +0100

    Added license & copyright notices

--- pull.go
+++ pull.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2016 Pulcy.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -10,8 +24,8 @@ var (
 	pullDockerRegistry string
 	pullCmd            = &cobra.Command{
 		Use:   "pull",
-		Short: "Pull an image from the arvika-ssh registry",
-		Long:  "Pull an image from the arvika-ssh registry",
+		Short: "Pull an image from the default registry",
+		Long:  "Pull an image from the default registry",
 		Run:   runPull,
 	}
 )
