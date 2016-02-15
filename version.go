08d198737064a35e7a1edebc3f46692cdacdd026
Ewout Prangsma <ewout@prangsma.net>
2016-02-15 15:29:31 +0100

    Added version command

---
+++ version.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	hdr = "   ___       __            " + "\n" +
+		"  / _ \\__ __/ /__ ___ _____" + "\n" +
+		" / ___/ // / (_-</ _ `/ __/" + "\n" +
+		"/_/   \\_,_/_/___/\\_,_/_/   "
+)
+
+var (
+	cmdVersion = &cobra.Command{
+		Use: "version",
+		Run: showVersion,
+	}
+)
+
+func init() {
+	mainCmd.AddCommand(cmdVersion)
+}
+
+func showVersion(cmd *cobra.Command, args []string) {
+	for _, line := range strings.Split(hdr, "\n") {
+		log.Info(line)
+	}
+	log.Info("%s %s, build %s\n", mainCmd.Use, projectVersion, projectBuild)
+}
